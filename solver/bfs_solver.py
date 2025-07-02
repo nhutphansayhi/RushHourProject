c493c9cc45aa655668db2766ecc454aea734644e
ngothoai <nhgthoai.0418@gmail.com>
2025-07-02 15:49:18 +0700

    State.to_string: convert state to string for easy comparation
    State.get_all_possible_moves
    + return list [(vehicle.id, direction)] (e.g: ("A", "LEFT"),...)
    
    State.is_valid_move: check for finding all possible moves
    
    State.move_vehicle: for each possible_move, we need to move vehicle to make new state
    
    --------------------
    BFS solver

--- solver/bfs_solver.py
+++ solver/bfs_solver.py
@@ -1,5 +1,38 @@
 from utils import Vehicle, State, import_map
 import queue
+import time
 
 def bfs_solver(initial_state):
+    start_time = time.time()
+    
+    if initial_state.is_solved():
+        return [], 0, 0
+    
     q = queue.Queue()
+    q.put((initial_state, []))  # (state, path)
+    visited = set()
+    nodes_expanded = 0
+    visited.add(initial_state.to_string())
+    while not q.empty():
+        current_state, path = q.get()
+        nodes_expanded += 1
+        possible_moves = current_state.get_all_possible_moves()
+        
+        for vehicle_id, direction in possible_moves:
+            new_state = current_state.move_vehicle(vehicle_id, direction)
+            
+            if new_state is None: 
+                continue
+                
+            state_string = new_state.to_string()
+            
+            if state_string not in visited:
+                visited.add(state_string)
+                new_path = path + [(vehicle_id, direction)]
+                
+                if new_state.is_solved():
+                    end_time = time.time()
+                    return new_path, nodes_expanded, end_time - start_time
+                
+                q.put((new_state, new_path))
+    return None, nodes_expanded, time.time() - start_time
