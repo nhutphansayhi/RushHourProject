d1af496fb31329c883b1c475f9bfb2f48450181d
PhacyLuz <ntphuc23@clc.fitus.edu.vn>
2025-07-12 18:01:25 +0700

    hoho

--- solver/dfs_solver.py
+++ solver/dfs_solver.py
@@ -28,28 +28,7 @@ def dfs_solver(start_state):
     node_expanded = [0]
     path = dfs_handler(start_state, [], visited, node_expanded)
 
-    while stack:
-        current_state, path = stack.pop()
-        node_expanded += 1
-        state_key = current_state.to_string()
-
-        if state_key in visited:
-            continue
-        visited.add(state_key)
-
-        if current_state.is_solved():
-            cost = 0
-            for vehicle_id, _ in path:
-                cost += current_state.get_vehicle_by_id(vehicle_id).length
-            return cost, node_expanded, path
-
-        if len(path) >= max_depth:
-            continue  # Prevent infinite loops
-
-        for move in current_state.get_all_possible_moves():
-            vehicle_id, direction = move
-            new_state = current_state.move_vehicle(vehicle_id, direction)
-            if new_state:
-                stack.append((new_state, path + [move]))
-
-    return None, 0, []
+    if path is not None:
+        return len(path), node_expanded[0], path
+    else:
+        return None, 0, []
